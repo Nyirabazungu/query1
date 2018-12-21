b467ea123fbd2b5c24726917aa2847b7cb9cb1b0
Nyirabazungu <Rutagarama1>
2018-12-21 16:11:56 +0200

    add header

--- JS/script.js
+++ JS/script.js
@@ -1,11 +1,26 @@
-$(document).ready(function() {
-    $("#quiz form").submit(function(event) {
-        var first= $("input#first").val();
-        var second= $("input#second").val();
-        $(".first").text(firstInput);
-        $(".second").text(secondInput);
+function myResult(){
+    var score= 0;
+    var ansArr=['e','c','a','a']
 
-        $(".submit").show();
-    })
-})
+var q1 = document.form["Quiz"]["q1"].value;
+var q2 = document.form["Quiz"]["q2"].value;
+var q3 = document.form["Quiz"]["q3"].value;
+var q4 = document.form["Quiz"]["q4"].value;
+ 
+ for (var i=1;i <=numOfQuestions;i++){
+     if(eval('q'+1)==''){
+         alert("you missed question number" +i)
+     }}
+     for(var i=1;i <= numOfQuestions;1++){
+         if (eval('q'+i) == AnsArr[i-1]){
+     score ++;
+         }
+     }
+ var results= document.getElementById( 'result')
+ results.innerHTML ="<h2> you score "+" point out of" + numOfQuestions +"</h2>"
+ alert('you scored' + score + 'out of' + numOfQuestions)
+    }
+
+
+
 
