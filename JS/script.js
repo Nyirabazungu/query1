03d58863a5dcb52e52dec172259eeebc8b9544fc
Nyirabazungu <Rutagarama1>
2018-12-21 17:30:21 +0200

    add footer

--- JS/script.js
+++ JS/script.js
@@ -1,29 +1,4 @@
-function myResult(){
-    var score= 0;
-
-var q1 = document.form["Quiz"]["q1"].value;
-var q2 = document.form["Quiz"]["q2"].value;
-var q3 = document.form["Quiz"]["q3"].value;
-var q4 = document.form["Quiz"]["q4"].value;
-
-if(q1=="e")
-{
-marks=marks+5;
-}
-if(q2=="c")
-{
-marks=marks+5;
-}
-if(q3=="a")
-{
-marks=marks+5;
-}
-if(q4=="a")
-{
-
-}
-alert("your result:"+score)
-    }
+l
 
 
 
