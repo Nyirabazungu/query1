0b57b55aa942f9184c5a4bdf588309868d449466
Nyirabazungu <Rutagarama1>
2019-01-07 17:21:17 +0200

    add link in index.html

--- JS/script.js
+++ JS/script.js
@@ -1,34 +1,3 @@
-/*$(document).ready(function myResult(){
-$('#blanks form').submit(function(event){
-
-var q1 = document.form["Quiz"]["q1"].value;
-var q2 = document.form["Quiz"]["q2"].value;
-var q3 = document.form["Quiz"]["q3"].value;
-var q4 = document.form["Quiz"]["q4"].value;
-var score=0;
-if(q1=="e")
-{
-score=score+5;
-}
-if(q2=="c")
-{
-score=score+5;
-}
-if(q3=="a")
-{
-score=score+5;
-}
-if(q4=="a")
-{
-score=score+5;
-}
-alert("your:"+score)
-$(".Output").show();
-$("#display").text("your marks is :"+ score);
-
-event.preventDefault();
-})
-});*/
 $(document).ready(function (){
     $('#blanks form').submit(function(event){
     var marks=0;
@@ -40,7 +9,7 @@ $(document).ready(function (){
                     }
                 })
                 
-          alert("your marks:"+ marks)
+          alert("your marks:"+ marks +"/20")
     event.preventDefault();
    });
    });
